bbfab9237a7aa46e4f51258b3183f0afa78fe7da
Sebastian Sanitz <sebastian@sanitz.net>
2010-07-11 03:23:58 +0200

    added icons for disabled projects and removed useless permission

--- status.js
+++ status.js
@@ -23,7 +23,7 @@ hudson.status = function() {
     function asIcon(result) {
         var icon = document.createElement('img'),
             name = result.color;
-        if (name == 'aborted') {
+        if (name === 'aborted' || name === 'disabled') {
             name = 'grey';
         }
         icon.src = name + ".gif";
